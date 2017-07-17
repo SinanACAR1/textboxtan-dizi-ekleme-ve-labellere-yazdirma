f91d951bd8704406ea04a44871ba3df79f306eda
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 23:23:36 +0300

    Add files via upload

---
+++ TEXTBOXTAN LİSTBOXA LİSTBOXTAN LABALLE DEĞER GÖNDERME/TEXTBOXTAN LİSTBOXA LİSTBOXTAN LABALLE DEĞER GÖNDERME/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+           
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+        int sayac = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = listBox1.Items[sayac].ToString();
+            sayac++;
+        }
+    }
+}
